d6468aaf16869ee2e9a86294f67f6743b23d304d
James <>
2023-10-19 22:02:20 -0500

    loops/arrays

--- JS/index.js
+++ JS/index.js
@@ -13,10 +13,42 @@ while (i < 5) {
 }
 
 //do-while loop
-//similar to a while loop, but the do block is seperated from code block
+//similar to a while loop, but the do block is seperated from conditional block
 let n = 0
 do {
     console.log("do loop", n);
     n++;
 }
 while (n < 5)
+
+
+
+//evens/odds
+//fizzbuzz
+for (i=1; i<=20; i++) {
+    if(i%2){
+        console.log(i + " odd")
+    } else {
+        console.log(i + " even")
+    }
+}
+
+//array with while
+const myArray = ["apple", "orange", "grape", "lemon"]
+
+let b = 0
+
+while(b<myArray.length) {
+    console.log(myArray[b]);
+
+    b++;
+}
+
+//do loop
+let userHeight
+do {
+    console.log("You must be at least 5 feet tall to join this ride")
+    userHeight = prompt("please enter your height")
+}
+while(userHeight<5);
+alert("Enjoy the ride!");
